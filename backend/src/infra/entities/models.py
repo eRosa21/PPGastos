e4853846e9bfbe5ad9b3099e245f1b290de64798
Esthéfano <rosaesthefano@gmail.com>
2026-07-31 08:02:35 -0300

    refactoring: redoing the foreign keys

--- backend/src/infra/entities/models.py
+++ backend/src/infra/entities/models.py
@@ -1,10 +1,11 @@
 from sqlalchemy import Column, Integer, String, Float, Date, ForeignKey
 from database.database import Base
+from sqlalchemy.orm import relationship
 
 class Banco(Base):
     __tablename__ = "bancos"
     id = Column("id",Integer, primary_key=True, index=True)
-    nome = Column("banco",String, nullable=False)
+    nome = Column("banco",String, unique=True, nullable=False)
     saldo = Column("saldo",Float, nullable=False)
     fatura = Column("fatura",Float, nullable=False)
     
@@ -16,8 +17,13 @@ class Banco(Base):
 class Gastos(Base):
     __tablename__ = "gastos"
     id = Column("id",Integer, primary_key=True, index=True)
+    nome = Column("nome",String, nullable=False)
+    valor = Column("valor",Float, nullable=False)
+    id_banco = Column("id_banco",Integer, ForeignKey("bancos.id"), nullable=False)
 
 class Caixinhas(Base):
     __tablename__ = "caixinhas"
     id = Column("id",Integer, primary_key=True, index=True)
-    
+    nome = Column("nome",String, nullable=False)
+    valor = Column("valor",Float, nullable=False)
+    id_banco = Column("id_banco",Integer, ForeignKey("bancos.id"), nullable=False)
