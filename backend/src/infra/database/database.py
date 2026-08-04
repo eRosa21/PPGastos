2ba08b07c9535ce29c43df62c3c7aa1c4f18a278
Esthéfano <rosaesthefano@gmail.com>
2026-08-04 08:09:59 -0300

    re-do location off the main

--- backend/src/infra/database/database.py
+++ backend/src/infra/database/database.py
@@ -2,8 +2,10 @@ import os
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from pathlib import Path
 from dotenv import load_dotenv
 
+PROJECT_ROOT = Path(__file__).resolve().parent.parent.parent
 load_dotenv() 
 
 DATABASE_URL = os.getenv("DATABASE_URL")
