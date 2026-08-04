9d1aad2d99f8a31a8d09a188ce07875838a06bec
Esthéfano <rosaesthefano@gmail.com>
2026-08-04 00:42:15 -0300

    wip: trying connect database with uvicorn

---
+++ backend/main.py
@@ -0,0 +1,25 @@
+import os
+import sys
+from pathlib import Path
+import uvicorn
+
+# Adiciona a pasta infra ao path, para que 'from database.database import ...' funcione
+INFRA_PATH = Path(__file__).resolve().parent / "src" / "infra"
+sys.path.append(str(INFRA_PATH))
+
+from fastapi import FastAPI,Depends,HTTPException
+from sqlalchemy.orm import Session
+from dotenv import load_dotenv
+from src.infra.database.database import get_db,engine,Base
+from src.infra.entities.models import Banco,Gastos,Caixinhas
+
+load_dotenv()
+
+Base.metadata.create_all(bind=engine)
+
+
+app = FastAPI()
+
+if __name__ == "__main__":
+    port = int(os.environ.get("API_PORT", 8000))
+     uvicorn.run("main:app", host="127.0.0.1", port=port, reload=True)
